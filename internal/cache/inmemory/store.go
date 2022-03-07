5ad6150ba5095a83ede2b29c0bd0a6139e186483
Ayush Rangwala <arangwala@vmware.com>
2022-03-07 17:11:07 +0530

    feat: Add cache store and interfaces

--- internal/cache/inmemory/store.go
+++ internal/cache/inmemory/store.go
@@ -1,62 +1,207 @@
 package inmemory
 
 import (
-	"fmt"
+	"context"
+	"sync"
 	"time"
 
-	cache "github.com/patrickmn/go-cache"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"golang.org/x/sync/errgroup"
+
+	"currency-converter/internal/cache"
+	apierrs "currency-converter/internal/errors"
+	"currency-converter/internal/exchange"
+	"currency-converter/internal/factory"
 )
 
-// CacheService handles in-memory caching of our rates.
-type CacheService struct {
-	client *Client
-	store  *cache.Cache
+var _ cache.Store = (*inMemory)(nil)
+
+// entry represents an entry of the inMemory cache
+type entry struct {
+	// data is the actual content to be stored in the cache.
+	data interface{}
+
+	// expiration is the calculated time based on the passed validity, till this entry is valid.
+	expiration time.Time
 }
 
-// NewCacheService creates a new handler for this service.
-func NewCacheService(
-	client *Client,
-	store *cache.Cache,
-) *CacheService {
-	return &CacheService{
-		client,
-		store,
+// newStoreEntry returns an entry from the data and validity, which is to be stored in the cache.
+func newStoreEntry(data interface{}, validity time.Duration) *entry {
+	e := &entry{
+		data: data,
 	}
-}
 
-// Get will return our in-memory stored currency/rates.
-func (s *CacheService) Get(base string, date time.Time) (*RateResponse, bool) {
-	if x, found := s.store.Get(getCacheKey(base, date)); found {
-		return x.(*RateResponse), found
+	if validity > 0 {
+		e.expiration = time.Now().Add(validity)
 	}
-	return nil, false
+
+	return e
 }
 
-// Store will cache our currency/rates in-memory.
-func (s *CacheService) Store(rsp *RateResponse, date time.Time) {
-	// Set a stored timestamp.
-	rsp.Timestamp = time.Now().Unix()
-
-	s.store.Set(
-		getCacheKey(rsp.Base, date),
-		rsp,
-		cache.DefaultExpiration,
-	)
-}
-
-// IsExpired checks whether the rate stored is expired.
-func (s *CacheService) IsExpired(base string, date time.Time) bool {
-	if _, found := s.store.Get(getCacheKey(base, date)); found {
+// IsExpired checks whether the entry stored is expired.
+func (e *entry) IsExpired() bool {
+	if e.expiration.IsZero() {
+		// no expiration set
 		return false
 	}
+
+	if time.Now().UnixNano() < e.expiration.UnixNano() {
+		return false
+	}
+
 	return true
 }
 
-// Expire will expire the cache for a given base currency.
-func (s *CacheService) Expire(base string, date time.Time) {
-	s.store.Delete(getCacheKey(base, date))
+// inMemory is the cache store where the data will be in-memory.
+type inMemory struct {
+	items map[string]*entry
+	mu    *sync.RWMutex
 }
 
-func getCacheKey(base string, date time.Time) string {
-	return fmt.Sprintf("%s_%s", base, date.Format("2006-01-02"))
+// NewStore is a constructor for inMemory cache store.
+func NewStore() cache.Store {
+	return &inMemory{
+		items: map[string]*entry{},
+		mu:    &sync.RWMutex{},
+	}
+}
+
+func (store *inMemory) AvailableCurrencies(exchangeProvider exchange.ProviderType) ([]string, error) {
+	store.mu.RLock()
+	defer store.mu.RUnlock()
+
+	if val, present := store.items[string(exchangeProvider)]; present {
+		return val.data.([]string), nil
+	}
+
+	var err error
+	var currencies []string
+
+	// cache miss, update the cache and return the value
+	provider := factory.NewExchangeRatesProviderFactory().BuildExchangeRatesProvider(exchangeProvider)
+	if currencies, err = provider.Currencies(); err != nil {
+		return []string{}, err
+	}
+
+	return currencies, store.SetAvailableCurrencies(exchangeProvider, currencies)
+}
+
+func (store *inMemory) SetAvailableCurrencies(exchangeProvider exchange.ProviderType, currencyCodes []string) error {
+	if currencyCodes == nil || len(currencyCodes) == 0 {
+		return apierrs.InvalidArgumentError
+	}
+
+	store.mu.Lock()
+	defer store.mu.Unlock()
+
+	store.items[string(exchangeProvider)] = newStoreEntry(currencyCodes, 2*7*24*time.Hour) // 2 weeks of validity
+
+	return nil
+}
+
+func (store *inMemory) GetExchangeRate(currencyCode string, exchangeProvider exchange.ProviderType) (float32, error) {
+	store.mu.RLock()
+	defer store.mu.RUnlock()
+
+	key := cache.GetKey(currencyCode, exchangeProvider)
+
+	if val, present := store.items[key]; present {
+		return val.data.(float32), nil
+	}
+
+	// cache MISS: refresh rates in cache
+	provider := factory.NewExchangeRatesProviderFactory().BuildExchangeRatesProvider(exchangeProvider)
+
+	var err error
+	var rates map[string]float32
+
+	if rates, err = provider.LiveRates(); err != nil {
+		return -1, apierrs.UpstreamExchangeRateServerError
+	}
+
+	if err = store.SetExchangeRate(currencyCode, exchangeProvider, rates[currencyCode], cache.DefaultExpiration); err != nil {
+		return -1, apierrs.InternalCacheError
+	}
+
+	val, present := rates[currencyCode]
+	if !present {
+		return -1, apierrs.CacheKeyNotFoundError
+	}
+
+	return val, nil
+}
+
+func (store *inMemory) SetExchangeRate(
+	currencyCode string,
+	exchangeProvider exchange.ProviderType,
+	rate float32,
+	expiration time.Duration) error {
+	store.mu.Lock()
+	defer store.mu.Unlock()
+
+	key := cache.GetKey(currencyCode, exchangeProvider)
+
+	store.items[key] = newStoreEntry(rate, expiration)
+
+	return nil
+}
+
+func (store *inMemory) RefreshExchangeRates(providers []exchange.ProviderType) error {
+	logger := logrus.New()
+
+	g, _ := errgroup.WithContext(context.Background())
+	workers := len(providers)
+
+	for i := 0; i < workers; i++ {
+		g.Go(func() error {
+			store.mu.Lock()
+			defer store.mu.Unlock()
+
+			provider := factory.NewExchangeRatesProviderFactory().BuildExchangeRatesProvider(exchangeProvider)
+
+			var err error
+			var rates map[string]float32
+
+			if rates, err = provider.LiveRates(); err != nil {
+				return err
+			}
+
+			for code, rate := range rates {
+				if tErr := store.SetExchangeRate(code, exchangeProvider, rate, cache.DefaultExpiration); tErr != nil {
+					if err == nil {
+						err = tErr
+						continue
+					}
+
+					err = errors.Wrapf(tErr,
+						"failed to set exchangerate for currency [%s] and provider [%s]", code, exchangeProvider)
+				}
+			}
+
+			return err
+		})
+	}
+
+	err := g.Wait()
+	if err != nil {
+		logger.WithError(err).Error("refresher workers stopped with error")
+	} else {
+		logger.Info("refresher workers stopped")
+	}
+	return err
+}
+
+// CleanupAllExpired will delete all the expired entries.
+func (store *inMemory) CleanupAllExpired() {
+	store.mu.Lock()
+	defer store.mu.Lock()
+
+	for key, cacheEntry := range store.items {
+		if !cacheEntry.IsExpired() {
+			continue
+		}
+
+		delete(store.items, key)
+	}
 }
--- internal/cache/interface.go
+++ internal/cache/interface.go
@@ -1,1 +1,53 @@
-package store
+package cache
+
+import (
+	"crypto/md5"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"currency-converter/internal/exchange"
+)
+
+const DefaultExpiration = 2 * time.Minute
+
+// Store holds the currency exchange rates for an exchange provider with the currency code.
+type Store interface {
+	// AvailableCurrencies returns all the available currencies from the cache for an exchange provider. (only on Cache Miss or lazy populating)
+	AvailableCurrencies(exchangeProvider exchange.ProviderType) ([]string, error)
+
+	// SetAvailableCurrencies sets the available currencies to the cache for an exchange provider.
+	SetAvailableCurrencies(exchangeProvider exchange.ProviderType, currencyCodes []string) error
+
+	// GetExchangeRate returns the exchange rate for the passed currency code.
+	// returns NotFound error if key not found.
+	GetExchangeRate(currencyCode string, exchangeProvider exchange.ProviderType) (float32, error)
+
+	// SetExchangeRate sets the exchange rate for a key from exchange provider and the currency code.
+	// By default, each rate will have an expiration of 2 minutes.
+	SetExchangeRate(currencyCode string, exchangeProvider exchange.ProviderType, rate float32, expiration time.Duration) error
+
+	// RefreshExchangeRates fetches the latest exchange rates from all the supported exchange rates providers.
+	// Will be used to refresh rates at:
+	// 1. "Cache Miss" in a request for that provider.
+	// 2. Every 5 minute refresh.
+	RefreshExchangeRates(providers []exchange.ProviderType) error
+
+	// CleanupAllExpired will cleanup all the entries which are expired.
+	// One of its usage is going to be in a background job running every 5 minute.
+	CleanupAllExpired()
+}
+
+// GetKey is the constructor for cache key using exchange provider and the currency code.
+// It will be used to set and get the rates.
+func GetKey(currencyCode string, exchangeProvider exchange.ProviderType) string {
+	jsonBytes, _ := json.Marshal(struct {
+		code     string
+		provider string
+	}{
+		code:     currencyCode,
+		provider: string(exchangeProvider),
+	})
+	md5Sum := md5.Sum(jsonBytes)
+	return fmt.Sprintf("%x", md5Sum[:])
+}
